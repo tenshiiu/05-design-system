9b3d068f8b08644220782c5dabd126c8490cbb39
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-10-02 17:24:58 -0300

    Story: Button

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -10,8 +10,12 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
+  stories: [
+    "../src/stories/**/*.stories.mdx", 
+    "../src/stories/**/*.stories.tsx)"
+  ],
+  
+    addons: [
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
